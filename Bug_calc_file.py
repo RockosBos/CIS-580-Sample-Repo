345e5f6c7e784f2a7567112b5e7d99da3d8e0bea
Meaghan Bryant <meg.marie.bryant@gmail.com>
2025-11-16 14:25:03 -0500

    Add initial calculator functions for add, subtract, divide, and multiply.

---
+++ Bug_calc_file.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+    #buggy code - returns subtract instead of add
+    return num1 - num2
+
+def subtract(num1, num2):
+    #buggy code - returns add instead of subtract
+    return num1 + num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def multiply(num1, num2):
+    return num1 * num2
