59221c34a0fa7376e2e5666b5ae1f00c5860bf2c
Meaghan Bryant <meg.marie.bryant@gmail.com>
2025-11-16 15:04:51 -0500

    Resolve issue with power function caused by typo - function was returning multiplication ininstead of power.

--- Bug_calc_file.py
+++ Bug_calc_file.py
@@ -11,8 +11,7 @@ def multiply(num1, num2):
     return num1 * num2
 
 def power(num1, num2):
-    #buggy code - returns multiplication instead of raising to power
-    return num1 * num2
+    return num1 ** num2
 
 def average(nums):
     #buggy code - total is not declared
