8a21d4eb59252f186100a2782f1d8c11e5c4afca
Meaghan Bryant <meg.marie.bryant@gmail.com>
2025-11-16 15:01:01 -0500

    Implement power and average functions.

--- Bug_calc_file.py
+++ Bug_calc_file.py
@@ -9,3 +9,13 @@ def divide(num1, num2):
 
 def multiply(num1, num2):
     return num1 * num2
+
+def power(num1, num2):
+    #buggy code - returns multiplication instead of raising to power
+    return num1 * num2
+
+def average(nums):
+    #buggy code - total is not declared
+    for num in nums:
+        total += num
+    return total / len(nums)
